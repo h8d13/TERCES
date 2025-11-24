7d689a8b9385aede9fc3bc059039aa0d60e971b0
h8d13 <hadean-eon-dev@proton.me>
2025-11-24 13:32:42 +0100

    add safe path unix check2

--- z/info.py
+++ z/info.py
@@ -7,10 +7,12 @@ from gnilux import (
     is_python,
     is_safe_path,
     username,
+    _debug,
 )
 
-print(f"venv: {is_venv}")
-print(f"python: {is_python}")
-print(f"user: {username} (safe: {is_safe_path(username)})")
+_debug(f"venv: {is_venv}")
+_debug(f"python: {is_python}")
+_debug(f"user: {username} (safe home path: {is_safe_path(username)})")
+
 filter_pattern = sys.argv[1] if len(sys.argv) > 1 else None
 info(filter_pattern=filter_pattern, device_index=CFG["device_index"])
